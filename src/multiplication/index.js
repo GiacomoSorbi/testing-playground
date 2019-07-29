9731943f328c80701cd6bf8245494c40f4a4f20d
Giacomo Sorbi <g.sorbi@gmail.com>
2019-07-29 23:24:23 +0100

    first setup with Jest and sample function completed

---
+++ src/multiplication/index.js
@@ -0,0 +1,3 @@
+const multiplication = (a, b) => a * b
+
+export default multiplication
---
+++ src/multiplication/multiplication.spec.js
@@ -0,0 +1,11 @@
+import multiplication from './'
+
+// main block - the "family" of tests
+describe('multiplication should', () => {
+  // sub blocks, all the features we want to test
+  it('exist', () => {
+    expect(multiplication).toBeDefined()
+  })
+
+  // add more here :)
+})
