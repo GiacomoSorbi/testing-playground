70523c6b6713a126272a2d4d729ef00cf0b4eb06
Giacomo Sorbi <g.sorbi@gmail.com>
2019-08-15 10:13:22 +0100

    added first fibonacciCounter tests

--- src/fibonacci/fibonacci.spec.js
+++ src/fibonacci/fibonacci.spec.js
@@ -1,4 +1,4 @@
-import fibonacci from './'
+import fibonacci, { fibonacciCounter } from './'
 
 describe('fibonacci should', () => {
   const seq = [0, 1, 1, 2, 3, 5]
@@ -8,4 +8,15 @@ describe('fibonacci should', () => {
       expect(fibonacci(i)).toBe(seq[i])
     })
   }
+})
+
+describe('fibonacciCounter should', () => {
+  const seq = [0, 1, 1, 2, 3, 5]
+
+  for (let i = 1; i < seq.length; i++)
+    it(`return requested number of first${i - 1 ? ' ' + i : ''} value${
+      i - 1 ? 's' : ''
+    } of Fibonacci sequence`, () => {
+      expect(fibonacciCounter(4)).toEqual(seq.slice(i))
+    })
 })
